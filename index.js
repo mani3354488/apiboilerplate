5b05a32b3392088e67617b537dd90c27b52f2347
mani3354488 <mani3354488@gmail.com>
2020-11-21 01:29:22 +0530

    final commit

--- index.js
+++ index.js
@@ -1,17 +1,44 @@
-const typeDefs = require('./src/graphql/schema');
-const resolvers = require('./src/graphql/resolver');
+import express from 'express';
+import {
+    ApolloServer,
+    gql
+} from 'apollo-server-express';
+import typeDefs from './graphql/schema';
+import resolvers from './graphql/resolvers';
+import db from './db';
 
-const express = require('express');
-const { ApolloServer, gql } = require('apollo-server-express');
+import faker from 'faker';
+import times from 'lodash.times';
+import random from 'lodash.random';
 
+const server = new ApolloServer({
+    typeDefs: gql(typeDefs),
+    resolvers,
+    context: { db }
+});
 
-
-const PORT = 4000;
 const app = express();
-const server = new ApolloServer({ typeDefs, resolvers })
+server.applyMiddleware({ app });
 
-server.applyMiddleware({app});
+db.sequelize.sync().then(() => {
+    // populate author table with dummy data
+    db.author.bulkCreate(
+        times(10, () => ({
+            firstName: faker.name.firstName(),
+            lastName: faker.name.lastName()
+        }))
+    );
 
-app.listen({ port: PORT }, () =>
+    // populate post table with dummy data
+    db.post.bulkCreate(
+        times(10, () => ({
+            title: faker.lorem.sentence(),
+            content: faker.lorem.paragraph(),
+            authorId: random(1, 10)
+        }))
+    );
+
+    app.listen({ port: 4000 }, () =>
         console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`)
     );
+});
--- readme.md
+++ readme.md
@@ -1,1 +1,3 @@
 This is kind of boiler plate for starting with node.js, express, graphql, with sequalize orm. 
+
+before starting make sure to create  db in postgres server and configure user correctly.
